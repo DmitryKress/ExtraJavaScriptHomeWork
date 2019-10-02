5cb0df694110aeb26679ccd153e75b655b8ad830
DmitryKress <dmitry_job@bk.ru>
2019-10-02 19:47:16 +0300

    1

---
+++ script.js
@@ -0,0 +1,15 @@
+var product = prompt("Please, type the first number");
+
+var product1 = prompt("Please, type the second number");
+
+if ((product > "50") & (product <= "99")) {
+  alert("nice");
+} else {
+  alert("Can not define, please try another value");
+}
+
+if ((product1 > "50") & (product1 <= "99")) {
+  alert("nice");
+} else {
+  alert("Can not define, please try another value");
+}
