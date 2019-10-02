e26d98917d3a9e004e880e06f5ea9a5899d1734f
DmitryKress <dmitry_job@bk.ru>
2019-10-02 20:52:00 +0300

    3

---
+++ script/script.js
@@ -0,0 +1,11 @@
+var firstProduct = prompt("Please, type the first number");
+
+var secondProduct = prompt("Please, type the second number");
+
+if (firstProduct >= "50" && firstProduct <= "99") {
+  alert("nice");
+} else if (secondProduct >= "50" && secondProduct <= "99") {
+  alert("nice");
+} else {
+  alert("Can not define, please try another value");
+}
