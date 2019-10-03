83541e6b0ad5cf276b2446901988829a011aa226
DmitryKress <dmitry_job@bk.ru>
2019-10-03 13:35:01 +0300

    4

---
+++ script3/script.js
@@ -0,0 +1,32 @@
+var product = prompt("Please,type a number");
+
+var xsNumber =
+  "0" &
+  "1" &
+  "2" &
+  "3" &
+  "4" &
+  "5" &
+  "6" &
+  "7" &
+  "8" &
+  "9" &
+  "10" &
+  "11" &
+  "12" &
+  "13" &
+  "14" &
+  "15" &
+  "16" &
+  "17" &
+  "18" &
+  "19" &
+  "20" &
+  "21" &
+  "22" &
+  "23" &
+  "24";
+
+if ((product = xsNumber)) {
+  alert("is extra small");
+}
